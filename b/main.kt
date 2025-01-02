15580552ec096ba78c170e753039c8a748f05ff5
kim4152 <andykimjhjh@gmail.com>
2025-01-02 16:58:04 +0900

    3장

--- b/main.kt
+++ b/main.kt
@@ -1,10 +1,12 @@
 package b
 
 import kotlinx.coroutines.*
+import kotlin.coroutines.EmptyCoroutineContext
 
 fun main(): Unit = runBlocking {
     name()
     launch(context = CoroutineName("myCoroutine")) { name() }
+    val a = EmptyCoroutineContext
 }
 
 fun name(){
